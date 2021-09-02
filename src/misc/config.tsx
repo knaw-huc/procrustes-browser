68ac0c1fa283ea1fd258a594be8a0ffa02316d4b
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2021-09-02 20:22:22 +0200

    Proxy added

--- src/misc/config.tsx
+++ src/misc/config.tsx
@@ -3,6 +3,4 @@ export const SERVICE_SERVER = "http://localhost/procrustes_service/";
 export const HOME = "http://localhost:4000";
 export const TIMBUCTOO_DATASET = "http://localhost:3000/details/";
 
-// Production
-//export const SERVICE_SERVER = "https://isidore.sd.di.huc.knaw.nl/isidore_service/";
-//export const HOME = "https://isidore.sd.di.huc.knaw.nl";
+
